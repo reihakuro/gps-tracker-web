cf63f962547dbf519393e0c2853ab0c93f5c690d
zRei <kientrungpham1910@gmail.com>
2026-04-14 20:07:40 +0700

    Add public files

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// File: firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+// 1. Khởi tạo lại Firebase trong luồng chạy ngầm
+firebase.initializeApp({
+    apiKey: "AIzaSyAwVMT-rUj-zWNuXR7Jy7WnM2dgVNv1VFY",
+    authDomain: "esp32-gps-tracker-f60da.firebaseapp.com",
+    databaseURL: "https://esp32-gps-tracker-f60da-default-rtdb.firebaseio.com",
+    projectId: "esp32-gps-tracker-f60da",
+    storageBucket: "esp32-gps-tracker-f60da.appspot.com",
+    messagingSenderId: "847848429624",
+    appId: "1:847848429624:web:b6d02dab7ddfebedd52ecd"
+});
+
+const messaging = firebase.messaging();
+
+// 2. Lắng nghe tin nhắn khi web ĐÃ BỊ TẮT
+messaging.onBackgroundMessage(function(payload) {
+    console.log('Đã nhận cảnh báo ngầm: ', payload);
+    
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/564/564276.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/564/564276.png',
+        vibrate: [500, 200, 500, 200, 500, 200, 500], // Rung bần bật
+        requireInteraction: true // Bắt buộc user bấm vào mới tắt thông báo
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
